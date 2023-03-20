ca3066d4d5a89969ef09cb20963b1679caadbfca
Ayush Kumar <kunsistent@Ayushs-MacBook-Air.local>
2023-03-20 19:57:54 +0530

    Some Changes

--- BankingAPI/urls.py
+++ BankingAPI/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('list_accounts/', list_accounts),
-    path('account/', account),
+    path('accounts/', account),
     path('transactions/', transactions),
 
 ]
