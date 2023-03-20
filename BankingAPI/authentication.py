b5db7a33a15fad96959f55f3a9074813ef9a0d8c
Jagteshver Singh <85994817+JS-CodeGamer@users.noreply.github.com>
2023-03-20 20:05:54 +0530

    refactor

--- BankingAPI/authentication.py
+++ BankingAPI/authentication.py
@@ -8,24 +8,25 @@ import jwt
 from rest_framework.response import Response
 import json
 
-get_user_api = ""
-
 class CustomAuthentication(BaseAuthentication):
     def authenticate(self, request):
-        auth_header = request.headers.get('Authorization')
-        if not auth_header:
-            raise AuthenticationFailed('Authorization header is missing')
-        
-        auth_token = auth_header.split(' ')[1]
-        
-        response = requests.get('http://localhost:8000/users/api/verify_token/',
-                            headers = {
-                                'Authorization': f'Bearer {auth_token}'
-                            }
-                        )
-        #user_response = user.decode(auth_token, settings.SECRET_KEY, algorithms = ['HS256'])
-        if response.ok :
-            return (json.loads(response.json()),None)
-        return (None,{"status_code" : response.status_code, "error":json.loads(response.json())})
-    
+        try:
+            auth_header = request.headers.get('Authorization')
+
+            if not auth_header:
+                raise AuthenticationFailed('Authorization header is missing')
+
+            response = requests.get(settings.USERS_MICROSERVICE_LINK,
+                                        headers = { 'Authorization': auth_header }
+                                    )
+
+            if response.ok :
+                user = json.loads(response.json())
+                return (user, None)
+
+            error = json.loads(response.json())
+            return (None, {"status_code" : response.status_code, "error" : error})
+
+        except Exception as e:
+            return (None, str(e))
 
